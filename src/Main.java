773ed59f594644eb58f81f5504b5c3e1d7ef5edb
Chearons <hearonca@mail.uc.edu>
2022-02-13 22:58:08 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	int age = 19;
+    int drinkingAge = 21;
+
+        System.out.println("Enter your age to get a drinking wrist band: " + age);
+
+        if(age >= 21)
+            System.out.println("You get a wrist band! ");
+        else;
+
+
+
+
+    }
+}
